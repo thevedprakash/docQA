c10a41fb19410c95411fed33acf3a2853357d1f7
Ved <thevedprakash.in@gmail.com>
2023-12-31 18:02:41 +0800

    Updating download.py for local model folder setup.

--- download.py
+++ download.py
@@ -1,12 +1,22 @@
 import sys
+import os
+
+
 from huggingface_hub import hf_hub_download
 
+model_path = 'models'
 
+# Check if the folder exists
+if not os.path.exists(model_path):
+    # If it does not exist, create it
+    os.makedirs(model_path)
+    print(f"The directory {model_path} was created.")
+else:
+    print(f"The directory {model_path} already exists.")
 # repo_id = sys.argv[0]
 # filename = sys.argv[1]
 
 repo_id="TheBloke/Llama-2-7B-Chat-GGUF"
 filename="llama-2-7b-chat.Q8_0.gguf"
 
-hf_hub_download(repo_id=repo_id, filename=filename)
-'/Users/ved/.cache/huggingface/hub/models--TheBloke--Llama-2-7B-Chat-GGUF/snapshots/191239b3e26b2882fb562ffccdd1cf0f65402adb/llama-2-7b-chat.Q8_0.gguf'
+hf_hub_download(repo_id=repo_id, filename=filename, local_dir=model_path ,local_dir_use_symlinks=False)
